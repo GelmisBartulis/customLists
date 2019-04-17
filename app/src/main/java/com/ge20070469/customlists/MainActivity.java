c24a2c454e2080fb265c50ec1775e65f4cfad8c8
Gelmis Bartulis <gelmis.bartulis@gmail.com>
2019-04-17 13:36:21 +0100

    first commit

---
+++ app/src/main/java/com/ge20070469/customlists/MainActivity.java
@@ -0,0 +1,39 @@
+package com.ge20070469.customlists;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<String> songs;
+    private String[] songName = {"Promiscuous", "Under The Bridge", "Sure Thing", "One, Two Step", "Californication", "In The Morning", "Glamorous", "'Till I Collapse"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, songName);
+        ListView listView = (ListView) findViewById(R.id.song_list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), ShowSong.class);
+                intent.putExtra("name",  i);
+                startActivity(intent);
+            }
+        });
+    }
+}
